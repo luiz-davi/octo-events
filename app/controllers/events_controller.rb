7f469f5f2fb464ee2c7ea78884518de5eea45149
Luiz Davi <luizd4398@gmail.com>
2022-07-05 23:13:39 -0300

    fix webhooks implementation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,14 @@
 class EventsController < ApplicationController
   def index
-    render json: events
+    render json: events, status: :ok
+  end
+
+  def create
+    if initialize_event.save
+      head :success 
+    else
+      head :unprocessable_entity
+    end
   end
 
   private
@@ -12,5 +20,12 @@ class EventsController < ApplicationController
     def events
       ::Event.where(number: number)
              .select("events.action, events.created_at")
+    end
+
+    def initialize_event
+      Event.new(
+        action: params[:action],
+        number: params[:number]
+      )
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  # Whitelist URLs for Ngrok
-  Rails.application.configure  do
-    config.hosts.clear
-  end
+  config.hosts << /[a-z0-9-]+\.ngrok\.io/
 end
